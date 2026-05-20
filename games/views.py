b005a246754371f683234d14b601c16edf06c5cc
Maksym Bykovets <236354@student.uek.krakow.pl>
2026-05-20 19:16:38 +0200

    Display games from database

--- games/views.py
+++ games/views.py
@@ -1,32 +1,8 @@
 from django.shortcuts import render
+from .models import Game
 
 
 def game_list(request):
-    games = [
-        {
-            'title': 'Cyberpunk 2077',
-            'genre': 'RPG',
-            'price': '59.99',
-            'tag': 'Hot',
-        },
-        {
-            'title': 'Minecraft',
-            'genre': 'Sandbox',
-            'price': '29.99',
-            'tag': 'Popular',
-        },
-        {
-            'title': 'Terraria',
-            'genre': 'Adventure',
-            'price': '9.99',
-            'tag': 'Sale',
-        },
-        {
-            'title': 'Stardew Valley',
-            'genre': 'Simulation',
-            'price': '14.99',
-            'tag': 'New',
-        },
-    ]
+    games = Game.objects.all().order_by('-created_at')
 
     return render(request, 'games/game_list.html', {'games': games})
