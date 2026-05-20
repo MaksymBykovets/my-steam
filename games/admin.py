3cdf19953aca51595b3fad333a6974b31636d47f
Maksym Bykovets <236354@student.uek.krakow.pl>
2026-05-20 17:15:59 +0200

    Add Game model and admin configuration

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Game
 
-# Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'genre', 'developer', 'price', 'release_date')
+    search_fields = ('title', 'genre', 'developer')
+    list_filter = ('genre', 'release_date')
--- games/models.py
+++ games/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    genre = models.CharField(max_length=50)
+    developer = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    release_date = models.DateField()
+    cover_image = models.ImageField(upload_to='game_covers/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
