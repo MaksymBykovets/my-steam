be2e2573f171012e692876cd8bd66958aace680c
Maksym Bykovets <236354@student.uek.krakow.pl>
2026-05-20 19:42:31 +0200

    Add game cover image support

--- my_steam/urls.py
+++ my_steam/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 def home(request):
@@ -28,3 +30,6 @@ urlpatterns = [
     path('games/', include('games.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
