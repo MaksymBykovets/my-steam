5c5b80956ad0d3e5fbb9f5bdc5108e04b2b35fb5
Maksym Bykovets <236354@student.uek.krakow.pl>
2026-05-20 19:23:17 +0200

    Add game detail page

--- games/views.py
+++ games/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Game
 
 
@@ -6,3 +6,9 @@ def game_list(request):
     games = Game.objects.all().order_by('-created_at')
 
     return render(request, 'games/game_list.html', {'games': games})
+
+
+def game_detail(request, pk):
+    game = get_object_or_404(Game, pk=pk)
+
+    return render(request, 'games/game_detail.html', {'game': game})
