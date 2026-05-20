41efdfce0d0c3c7b6fa5f5e0c1a6783bcdcc4cc4
Maksym Bykovets <236354@student.uek.krakow.pl>
2026-05-20 16:41:58 +0200

    Add games catalog page

--- games/views.py
+++ games/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def game_list(request):
+    games = [
+        {
+            'title': 'Cyberpunk 2077',
+            'genre': 'RPG',
+            'price': '59.99',
+            'tag': 'Hot',
+        },
+        {
+            'title': 'Minecraft',
+            'genre': 'Sandbox',
+            'price': '29.99',
+            'tag': 'Popular',
+        },
+        {
+            'title': 'Terraria',
+            'genre': 'Adventure',
+            'price': '9.99',
+            'tag': 'Sale',
+        },
+        {
+            'title': 'Stardew Valley',
+            'genre': 'Simulation',
+            'price': '14.99',
+            'tag': 'New',
+        },
+    ]
+
+    return render(request, 'games/game_list.html', {'games': games})
